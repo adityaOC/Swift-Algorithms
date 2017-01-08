1f22f0aa25368f01c1910aaea8acead72851b238
Aditya Gaonkr <adityagaonkr@Adityas-MacBook-Pro.local>
2017-01-07 19:40:59 -0600

    increment a number given in array

---
+++ Arrays/Arraya-Algorithms/Arraya-Algorithms/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Arraya-Algorithms
+//
+//  Created by Aditya Gaonkr on 07/01/17.
+//  Copyright © 2017 Aditya Gaonkr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        var array = [9,0,9]
+        increamentIntegerGivenInArray(array: &array)
+        
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func increamentIntegerGivenInArray(array: inout [Int])  {
+        
+        let size = array.count - 1
+        array[size] = array[size] + 1
+        
+        
+        for i in (1...size).reversed() where array[i] == 10{
+        print(i)
+            array[i] = 0
+            array[i-1] = array[i-1] + 1
+            
+        }
+        
+        //check for the carry
+        if array[0] == 10 {
+            array[0] = 0
+            array.insert(1, at: 0)
+        }
+        
+        print(" \naddition result = \(array)\n")
+        
+        
+    }
+}
+
